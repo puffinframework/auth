faedcc153ddaeaab5913d7df79fc0022d3783288
dfreire <dario.freire@gmail.com>
2014-12-07 12:58:03 +0000

    update

--- auth.go
+++ auth.go
@@ -1,10 +1,26 @@
 package auth
 
+import (
+	"fmt"
+)
+
+type UsersByID map[string]*User
+
 type User struct {
+	ID         string
 	Email      string
 	Hash       string
 	HashedPass string
 }
 
-func SignUp(email string, password string) {
+func SignUp(email string, password string, usersByID UsersByID) error {
+	if usersByID[email] != nil {
+		return fmt.Errorf("Email exists.")
+	}
+
+	usersByID[email] = &User{
+		ID:    email,
+		Email: email,
+	}
+	return nil
 }

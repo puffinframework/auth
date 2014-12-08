5dcdf73466c88876dfb0081c2919b978477ccb58
dfreire <dario.freire@gmail.com>
2014-12-08 14:20:41 +0000

    update

--- auth.go
+++ auth.go
@@ -3,38 +3,44 @@ package auth
 import (
 	"errors"
 	"github.com/puffinframework/event"
-	"github.com/puffinframework/snapshot"
-	"time"
 )
 
 var (
-	ErrEmailExists error = errors.New("auth: email exists")
+	ErrEmailUsed error = errors.New("auth: the email is already being used")
 )
 
-type UsedEmailsMap map[string]bool
-
-type UsersByID map[string]User
-
 type User struct {
+	AppId      string
 	Id         string
 	Email      string
 	Hash       string
 	HashedPass string
 }
 
+type UsersById map[string]User
+
+type AppIdByEmail map[string]string
+
 type CreatedUserEvent struct {
 	Header event.Header
 	Data   User
 }
 
-func CreateUser(email string, password string, emailsMap UsedEmailsMap) (CreatedUserEvent, error) {
-	if emailsMap[email] != nil {
-		return CreatedUserEvent{}, ErrEmailExists
+func CreateUser(appId string, email string, password string, appIdByEmail AppIdByEmail) (CreatedUserEvent, error) {
+	if appIdByEmail[email] == appId {
+		return CreatedUserEvent{}, ErrEmailUsed
 	}
 
 	evt := CreatedUserEvent{
-		Header: event.NewEvent("CreatedUser", 1),
-		Data:   User{Id: email, Email: email},
+		Header: event.NewHeader("CreatedUser", 1),
+		Data:   User{AppId: appId, Id: email, Email: email},
 	}
 	return evt, nil
+}
+
+func OnCreatedUser(evt CreatedUserEvent, appIdByEmail AppIdByEmail, usersById UsersById) error {
+	user := evt.Data
+	appIdByEmail[user.Email] = user.AppId
+	usersById[user.Id] = user
+	return nil
 }

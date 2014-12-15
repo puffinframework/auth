b9305b8003a82c17513dcecf3d7261630cb13f9f
dfreire <dario.freire@gmail.com>
2014-12-15 19:44:51 +0000

    update

--- auth_test.go
+++ auth_test.go
@@ -4,23 +4,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/puffinframework/auth"
 	"github.com/puffinframework/config"
+	"github.com/puffinframework/event"
+	"github.com/puffinframework/snapshot"
 	"github.com/stretchr/testify/assert"
 )
-
-type authConfig struct {
-	EventStore struct {
-		LeveldbDir string
-	}
-	SnapshotStore struct {
-		LeveldbDir string
-	}
-}
 
 func Test(t *testing.T) {
 	os.Setenv(config.ENV_VAR_NAME, config.MODE_TEST)
 
-	cfg := &authConfig{}
-	config.MustReadConfig(cfg)
-	assert.NotNil(t, cfg)
+	eventStore := event.NewLeveldbStore()
+	snapshotStore := snapshot.NewLeveldbStore()
+
+	authService := auth.NewAuth(eventStore, snapshotStore)
+	assert.NotNil(t, authService)
 }

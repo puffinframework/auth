37aacf2b0ae5aa6ba7cb0f53725397ab8259e4a8
dfreire <dario.freire@gmail.com>
2015-01-03 01:11:19 +0000

    update

--- jwt_test.go
+++ jwt_test.go
@@ -4,11 +4,33 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/puffinframework/auth"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestJWT(t *testing.T) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims["data"] = "a b c"
+	tokenStr, err := token.SignedString([]byte("right key"))
+	assert.Nil(t, err)
+
+	token1, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte("right key"), nil
+	})
+	assert.Nil(t, err)
+	assert.True(t, token1.Valid)
+	assert.Equal(t, "a b c", token1.Claims["data"])
+
+	token2, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte("wrong key"), nil
+	})
+	assert.NotNil(t, err)
+	assert.False(t, token2.Valid)
+	assert.Equal(t, "a b c", token2.Claims["data"])
+}
+
+func TestSessionEncoding(t *testing.T) {
 	userId := "user-1"
 	createdAt := time.Unix(123, 0)
 

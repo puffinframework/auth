bbeba01415afbf74c53d276d56b02d2da9e0dd49
dfreire <dario.freire@gmail.com>
2015-01-10 17:36:35 +0000

    update

--- data.go
+++ data.go
@@ -2,11 +2,31 @@ package auth
 
 import (
 	"time"
+
+	"github.com/puffinframework/snapshot"
 )
+
+type SnapshotData interface {
+}
+
+type impl struct {
+	ss   *snapshot.Store
+	data *snapshotData
+}
 
 type snapshotData struct {
 	LastEventDt          time.Time
 	UserById             UserById
 	UserIdByEmail        UserIdByEmail
 	VerificationByUserId VerificationByUserId
+}
+
+func NewSnapshotData(ss snapshot.Store) SnapshotData {
+	return nil
+}
+
+func Load() {
+}
+
+func Save() {
 }

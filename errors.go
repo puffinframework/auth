62652b493975fb68a745edda2cee804de7db06e3
dfreire <dario.freire@gmail.com>
2015-03-11 19:32:30 +0000

    update

--- errors.go
+++ errors.go
@@ -5,11 +5,11 @@ import (
 )
 
 var (
-	ErrEmailAlreadyUsed     error = errors.New("auth: the email is already being used")
-	ErrEmailNotVerified     error = errors.New("auth: the email is not verified")
-	ErrVerificationDenied   error = errors.New("auth: verification denied")
-	ErrSignInDenied         error = errors.New("auth: sign in denied")
-	ErrChangePasswordDenied error = errors.New("auth: change password denied")
-	ErrResetPasswordDenied  error = errors.New("auth: reset password denied")
-	ErrJwtNotValid          error = errors.New("auth: JWT is not valid")
+	ErrJwtNotValid          error = errors.New("AUTH-00")
+	ErrEmailAlreadyUsed     error = errors.New("AUTH-01")
+	ErrEmailNotVerified     error = errors.New("AUTH-02")
+	ErrVerificationDenied   error = errors.New("AUTH-03")
+	ErrSignInDenied         error = errors.New("AUTH-04")
+	ErrChangePasswordDenied error = errors.New("AUTH-05")
+	ErrResetPasswordDenied  error = errors.New("AUTH-06")
 )

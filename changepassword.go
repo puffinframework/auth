46fb28e0d0da9a5f80816c4b33c5116b428bdc4c
dfreire <dario.freire@gmail.com>
2015-03-28 17:38:30 +0000

    update

--- changepassword.go
+++ changepassword.go
@@ -9,10 +9,13 @@ type ChangedPasswordEvent struct {
 	Header event.Header
 	Data   ChangedPasswordEventData
 }
+
 type ChangedPasswordEventData struct {
 	UserId         string
 	HashedPassword []byte
 }
+
+type ChangedUserPasswordEvent ChangedPasswordEvent
 
 func ChangePassword(session Session, oldPassword string, newPassword string, sd SnapshotData) (ChangedPasswordEvent, error) {
 	hashedPassword := sd.GetHashedPassword(session.UserId)
@@ -33,12 +36,32 @@ func ChangePassword(session Session, oldPassword string, newPassword string, sd 
 	return evt, nil
 }
 
-func ChangeUserPassword(session Session, authorizationId string, userId string, newPassword string, sd SnapshotData) (ChangedPasswordEvent, error) {
-	// TODO
-	return ChangedPasswordEvent{}, nil
+func OnChangedPassword(evt ChangedPasswordEvent, sd SnapshotData) error {
+	data := evt.Data
+	sd.SetHashedPassword(data.UserId, data.HashedPassword)
+	return nil
 }
 
-func OnChangedPassword(evt ChangedPasswordEvent, sd SnapshotData) error {
+func ChangeUserPassword(session Session, authorizationId string, userId string, newPassword string, sd SnapshotData) (ChangedUserPasswordEvent, error) {
+	authorization := sd.GetUserAuthorization(session.UserId, authorizationId)
+	if !sd.IsSuperUser(session.UserId) || authorization.UserId == "" || !authorization.IsAuthorized {
+		return ChangedUserPasswordEvent{}, ErrNotAuthorized
+	}
+
+	newHashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), 10)
+	if err != nil {
+		return ChangedUserPasswordEvent{}, err
+	}
+
+	evt := ChangedUserPasswordEvent{
+		Header: event.NewHeader("ChangedPassword", 1),
+	}
+	evt.Data.UserId = session.UserId
+	evt.Data.HashedPassword = newHashedPassword
+	return evt, nil
+}
+
+func OnChangedUserPassword(evt ChangedUserPasswordEvent, sd SnapshotData) error {
 	data := evt.Data
 	sd.SetHashedPassword(data.UserId, data.HashedPassword)
 	return nil

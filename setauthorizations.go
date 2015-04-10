7f186ad94fe7e9baa312a060284213818cd394e3
dfreire <dario.freire@gmail.com>
2015-04-10 18:01:33 +0100

    update

--- setauthorizations.go
+++ setauthorizations.go
@@ -1,6 +1,18 @@
 package auth
 
 func (self *authServiceImpl) SetAuthorizations(sessionToken, authorizationId string, userIds []string, authorizationIds []string, IsAuthorized bool) error {
+	sd := self.processEvents()
+
+	session, err := DecodeSession(sessionToken)
+	if err != nil {
+		return err
+	}
+
+	authorization := sd.GetUserAuthorization(session.UserId, authorizationId)
+	if !authorization.IsAuthorized {
+		return ErrNotAuthorized
+	}
+
 	// TODO
 	return nil
 }

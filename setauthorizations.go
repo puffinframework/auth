4a5a8868e63b5705db49b0e1234566e08d26011e
dfreire <dario.freire@gmail.com>
2015-04-11 12:07:49 +0100

    update

--- setauthorizations.go
+++ setauthorizations.go
@@ -1,7 +1,31 @@
 package auth
 
+import (
+	"github.com/puffinframework/event"
+)
+
+type SetAuthorizationsEvent struct {
+	Header event.Header
+	Data   struct {
+		UserIds          []string
+		AuthorizationIds []string
+		IsAuthorized     bool
+	}
+}
+
 func (self *authServiceImpl) SetAuthorizations(sessionToken, authorizationId string, userIds []string, authorizationIds []string, IsAuthorized bool) error {
+	sd := self.processEvents()
+
+	session, err := DecodeSession(sessionToken)
+	if err != nil {
+		return err
+	}
+
+	authorization := sd.GetUserAuthorization(session.UserId, authorizationId)
+	if !authorization.IsAuthorized {
+		return ErrNotAuthorized
+	}
+
 	// TODO
 	return nil
 }
-
